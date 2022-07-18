43f57282c121a674df53eab9a39cfeac62deedd0
Anthony Aniobi <anthonyaniobi198@gmail.com>
2022-07-18 07:18:56 +0100

    created pages containers and added readme description

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # GPT3
 
 An Ai website built using only Css and react icons
+
+## Screenshot
+![Modern UI/UX GPT-3](https://i.ibb.co/TR5LW9z/image.png)
+
+## UI/UX
+[figma link](https://www.figma.com/file/lz9lLpFHMxHm2odnwM3R0z/gpt3?node-id=0%3A1)
--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+
 const App = () => {
     return (
-        <div>App</div>
+        <div className='App'>
+            <div className='gradient__bg'>
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
+        </div>
     )
 }
 
